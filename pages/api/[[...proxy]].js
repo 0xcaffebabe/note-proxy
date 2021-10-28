fa5aafff30d141089fd3edd6057ff9c22f2ae801
cjiping <cjiping@linewell.com>
2021-10-28 15:28:34 +0800

    uri编码

--- pages/api/[[...proxy]].js
+++ pages/api/[[...proxy]].js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default async function handler(req, res) {
-  const filename = req.query.proxy.join('/')
+  const filename = encodeURI(req.query.proxy.join('/'))
   res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Method': '*'})
   res.write(await (await fetch('https://github.com/0xcaffebabe/note/raw/gh-pages/' + filename)).text())
   res.end()

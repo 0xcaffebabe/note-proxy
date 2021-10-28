790632148c359bed187384e479d5c89bbbb249c5
cjiping <cjiping@linewell.com>
2021-10-28 15:21:32 +0800

    实现

---
+++ pages/api/[[...proxy]].js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function handler(req, res) {
+  const filename = req.query.proxy.join('/')
+  res.writeHead(200, {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Method': '*'})
+  res.write(await (await fetch('https://github.com/0xcaffebabe/note/raw/gh-pages/' + filename)).text())
+  res.end()
+}
